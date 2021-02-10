2549ef7281d013bf20c58017d23c2e53c214a860
Gustavo P. Santana <gustavopsantana4@gmail.com>
2021-02-10 16:25:15 -0300

    adicionando estilo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function HomePage() {
     return (
         <div>
-            <h1>Bem vindo ao meu website!</h1>
+            <h1 className="welcome">Bem vindo ao meu website!</h1>
         </div>
     )
 }

d21b4e7a584a5488453a134909f7767c64c2ba4a
Gustavo P. Santana <gustavopsantana4@gmail.com>
2021-02-11 00:30:27 -0300

    manutenção

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import React from 'react'
+
 function HomePage() {
     return (
         <div>
             <h1 className="welcome">Bem vindo ao meu website!</h1>
+            <div className="center">
+                <img className="eng" src={require('../img/icon_engrenagem.png')} alt="engrenagem"/>
+                <h1>Estamos em desenvolvimento...</h1>
+                <h2>Por favor, fique calmo. Voltaremos em breve!</h2>
+            </div>
         </div>
     )
 }
-  
+
 export default HomePage

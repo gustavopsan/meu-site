fe7dbe4af71dd2c48b76471c0fe7cf93ed69513e
Gustavo P. Santana <gustavopsantana4@gmail.com>
2021-02-11 10:54:09 -0300

    Ajustes para mobile

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function HomePage() {
         <div>
             <h1 className="welcome">Bem vindo ao meu website!</h1>
             <div className="center">
-                <img className="eng" src={require('../img/icon_engrenagem.png')} alt="engrenagem"/>
+                <a href="/"><img className="eng" src={require('../img/icon_engrenagem.png')} alt="engrenagem"/></a>
                 <h1>Estamos em desenvolvimento...</h1>
                 <h2>Por favor, fique calmo. Voltaremos em breve!</h2>
             </div>

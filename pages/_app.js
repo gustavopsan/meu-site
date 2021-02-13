6be9c9c0f38816b9790c146dbebf733d1fc070cf
Gustavo P. Santana <gustavopsantana4@gmail.com>
2021-02-12 23:28:14 -0300

    correção na importação do css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../css/footer.css'
-import '../css/Header.css'
-import '../css/Menu.css'
+import '../css/header.css'
+import '../css/menu.css'
 
 export default function App({ Component, pageProps}) {
     return <Component {...pageProps} />

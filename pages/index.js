4774ecd1d4a9f3aae9bb563589e77ad6586a22c8
Gustavo P. Santana <gustavopsantana4@gmail.com>
2021-02-13 12:12:26 -0300

    GSC Fix #1

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 import Footer from '../components/footer'
 import Header from '../components/header'
@@ -6,6 +7,10 @@ import Header from '../components/header'
 function HomePage() {
     return (
         <>
+            <Helmet>
+                <title>Home - Gustavo P. Santana</title>
+                <meta name="google-site-verification" content="bjjqbRgiN9TaBYQsLrq0RhcefY6c6erwR1XHd9urXq0" />
+            </Helmet>
             <Header />
             <h1>Dessa vez temos uma homepage, ou quase...</h1>
             <Footer />

f5889a4f42d1aae3dad6c2cb6e3d70a0687b691a
Gustavo Santana <gustavo.santana@mundiware.com>
2021-07-07 13:27:22 -0300

    Add Google Adsense Meta (Fix #1)

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ function HomePage() {
     return (
         <>
             <Helmet>
+                <script data-ad-client="ca-pub-4865440295689037" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <title>Gustavo P. Santana - Desenvolvedor Frontend</title>
                 <link rel="apple-touch-icon" sizes="180x180" href={require('../assets/icons/apple-touch-icon.png')} />
                 <link rel="icon" type="image/png" sizes="32x32" href={require('../assets/icons/favicon-32x32.png')} />
@@ -16,7 +17,6 @@ function HomePage() {
                 <link rel="manifest" href="/site.webmanifest" />
                 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
                 <meta name="description" content="Website de Desenvolvedor" />
-                <script data-ad-client="ca-pub-4865440295689037" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             </Helmet>
             <Header />
             <Container bg_img={require('../assets/img/yellow_pen.jpg')}>
